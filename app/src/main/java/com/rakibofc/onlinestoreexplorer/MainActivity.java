d11a70fab33889b185e4076fc90fa35bed40028f
Rakibul Islam <rakibull2000@gmail.com>
2023-09-18 01:03:15 +0600

    Add store id textview with UI design in store_item

--- app/src/main/java/com/rakibofc/onlinestoreexplorer/MainActivity.java
+++ app/src/main/java/com/rakibofc/onlinestoreexplorer/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.rakibofc.onlinestoreexplorer.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.rvStoreList.setAdapter(new StoreAdapter(190));
     }
 }
