b159f59614d00f496c1e5354c09d59f68fce7132
Rakibul Islam <rakibull2000@gmail.com>
2023-09-18 23:54:41 +0600

    Add dummy data in MainActivity

--- app/src/main/java/com/rakibofc/onlinestoreexplorer/ui/MainActivity.java
+++ app/src/main/java/com/rakibofc/onlinestoreexplorer/ui/MainActivity.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.rakibofc.onlinestoreexplorer.R;
 import com.rakibofc.onlinestoreexplorer.adapter.StoreAdapter;
 import com.rakibofc.onlinestoreexplorer.databinding.ActivityMainBinding;
+import com.rakibofc.onlinestoreexplorer.model.Store;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +21,17 @@ public class MainActivity extends AppCompatActivity {
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.rvStoreList.setAdapter(new StoreAdapter(190));
+        binding.rvStoreList.setAdapter(new StoreAdapter(getStoreDataList()));
+    }
+
+    private List<Store> getStoreDataList() {
+
+        List<Store> storeDataList = new ArrayList<>();
+
+        for (int i = 1; i <= 190; i++) {
+
+            storeDataList.add(new Store(i, getString(R.string.store_name_text), getString(R.string.store_address_text)));
+        }
+        return storeDataList;
     }
 }
